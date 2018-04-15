d17dbea4bddb3314451d2ee75e4885ec906da0fc
mzipper <markzipper99@gmail.com>
2018-04-15 17:13:09 -0400

    enabled user to change snake speed -currently during game play. Some code adjustments. (minor formatting changes.)

--- src/main/java/com/zipper/snake/Main.java
+++ src/main/java/com/zipper/snake/Main.java
@@ -1,12 +1,11 @@
 package com.zipper.snake;
-import javax.swing.JFrame;
 
 public class Main {
 
 	public static void main(String[] args) {
 
 		//Creating the window with all its awesome snaky features
-		Window f1= new Window();
+		new Window();
 		
 		
 
--- src/main/java/com/zipper/snake/Window.java
+++ src/main/java/com/zipper/snake/Window.java
@@ -1,60 +1,71 @@
 package com.zipper.snake;
-import java.awt.GridLayout;
-import java.awt.event.KeyListener;
+
 import java.util.ArrayList;
 
 import javax.swing.JFrame;
-
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 class Window extends JFrame{
 	private static final long serialVersionUID = -2542001418764869760L;
 	public static ArrayList<ArrayList<DataOfSquare>> Grid;
 	public static int width = 20;
 	public static int height = 20;
-	public Window(){
+	
+	private JPanel screenPanel;
+	
+	private JPanel speedButtonsPanel;
+	private JRadioButton rBLowSpeed;
+	private JRadioButton rBMedSpeed;
+	private JRadioButton rBHiSpeed;
+	private ButtonGroup group;
+	
+	
+	public Window()
+	{
+		setLayout(new BorderLayout());
+		setMinimumSize(new Dimension(410, 300) ); //look into panel setting abilities..
 		
+		buildScreenPanelPlus();
+		add(screenPanel, BorderLayout.CENTER);
 		
-		// Creates the arraylist that'll contain the threads
-		Grid = new ArrayList<ArrayList<DataOfSquare>>();
-		ArrayList<DataOfSquare> data;
+		buildSpeedRadioButtons();
+	    add(speedButtonsPanel, BorderLayout.WEST);
+	    
+		// Links the window to the keyboardlistenner.
+		screenPanel.addKeyListener((KeyListener) new KeyboardListener());
 		
-		// Creates Threads and its data and adds it to the arrayList
-		for(int i=0;i<width;i++){
-			data= new ArrayList<DataOfSquare>();
-			for(int j=0;j<height;j++){
-				DataOfSquare c = new DataOfSquare(2);
-				data.add(c);
-			}
-			Grid.add(data);
-		}
+		//gives the screenPanel back the keyboard focus so that arrows can control the snake.
+		//screenPanel.requestFocusInWindow();
+		//screenPanel.setFocusable(true);			//needed to give focus to screenPanel 
+	    addMouseListener(new MouseEventDemo());	//needed to return focus to screenPanel
 		
-		// Setting up the layout of the panel
-		getContentPane().setLayout(new GridLayout(20,20,0,0));
-		
-		// Start & pauses all threads, then adds every square of each thread to the panel
-		for(int i=0;i<width;i++){
-			for(int j=0;j<height;j++){
-				getContentPane().add(Grid.get(i).get(j).square);
-			}
-		}
-		
-		// initial position of the snake
+	    // initial position of the snake
 		Tuple position = new Tuple(10,10);
 		// passing this value to the controller
 		ThreadsController c = new ThreadsController(position);
+	    
+		//Make screenPanel get the [initial focus.] focus whenever window is activated.
+		addWindowFocusListener(new WindowAdapter() {
+		    public void windowGainedFocus(WindowEvent e) {
+		        screenPanel.requestFocusInWindow();
+		    }
+		});
+	   
+		//c.setSpeed(100);
 		//Let's start the game now..
 		c.start();
 
-		// Links the window to the keyboardlistenner.
-		this.addKeyListener((KeyListener) new KeyboardListener());
+		
 		
 		//Setting up the window settings
 		setTitle("Snake");
-		setSize(300,300);
-		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setLocationRelativeTo(null); //Center window on screen.
 		
+		pack();
+		setLocationRelativeTo(null); //Center window on screen.
+		setVisible(true);
 		
 		//To do : handle multiplayers .. The above works, test it and see what happens
 		
@@ -63,4 +74,127 @@ class Window extends JFrame{
 		//c2.start();
 		
 	}
-}
+	
+	private void buildScreenPanelPlus()
+	{
+		screenPanel = new JPanel();
+		screenPanel.setMinimumSize(new Dimension(300,300)); //look into panel setting abilities..
+		
+		// Creates the arraylist that'll contain the threads
+		Grid = new ArrayList<ArrayList<DataOfSquare>>();
+		ArrayList<DataOfSquare> data;
+		
+		// Creates Threads and its data and adds it to the arrayList
+		for(int i=0;i<width;i++)
+		{
+			data= new ArrayList<DataOfSquare>();
+			for(int j=0;j<height;j++)
+			{
+				DataOfSquare c = new DataOfSquare(2);
+				data.add(c);
+			}
+			Grid.add(data);
+		}
+				
+		// Setting up the layout of the panel
+		screenPanel.setLayout(new GridLayout(20,20,0,0));
+		// Start & pauses all threads, then adds every square of each thread to the panel
+		for(int i=0;i<width;i++)
+		{
+			for(int j=0;j<height;j++)
+			{
+				screenPanel.add(Grid.get(i).get(j).square);
+			}
+		}
+				
+	} //end of buildScreenPanelPlus method
+	
+	private void buildSpeedRadioButtons()
+	{
+		speedButtonsPanel = new JPanel();
+		
+		//Set some Settings to Panel.
+		speedButtonsPanel.setLayout(new GridLayout(3,1));
+		speedButtonsPanel.setBorder(BorderFactory.createTitledBorder("Choose Snake Speed"));
+	
+		//Create RadioButtons.
+		rBLowSpeed 	= new JRadioButton("Low Speed");
+		rBMedSpeed 	= new JRadioButton("Medium Speed");
+		rBHiSpeed 	= new JRadioButton("High Speed (Original)");
+		
+		//Add RadioButtons to a Group.
+		group = new ButtonGroup();
+		group.add(rBLowSpeed);
+		group.add(rBMedSpeed);
+		group.add(rBHiSpeed);
+	
+		rBHiSpeed.doClick();
+	
+		//Add RadioButtons to the Panel.
+		speedButtonsPanel.add(rBLowSpeed);
+		speedButtonsPanel.add(rBMedSpeed);
+		speedButtonsPanel.add(rBHiSpeed);
+	
+		rBLowSpeed.addActionListener(new RadioButtonListener());
+		rBMedSpeed.addActionListener(new RadioButtonListener());
+		rBHiSpeed.addActionListener(new RadioButtonListener());
+	
+	} // end of buildSpeedRadioButtons method.
+	
+	//class enables radio buttons to work.
+	private class RadioButtonListener implements ActionListener
+	{
+
+		@Override
+		public void actionPerformed(ActionEvent e)
+		{
+			if(rBLowSpeed.isSelected())
+			{
+				ThreadsController.setSpeed(110);
+			}
+			else if(rBMedSpeed.isSelected())
+			{
+				ThreadsController.setSpeed(80);
+			}
+			else if(rBHiSpeed.isSelected())
+			{
+				ThreadsController.setSpeed(50);
+			}
+		}
+		
+	} //end of RadioButtonListener class
+	
+	//class that gives the screenPanel back the keyboard focus so that arrows can control the snake.
+	public class MouseEventDemo implements MouseListener
+	{
+		 public void mouseClicked(MouseEvent e) {
+		        //Since the user clicked on us, let us get focus!
+		        screenPanel.requestFocusInWindow();
+		    }
+
+		@Override
+		public void mouseEntered(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseExited(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mousePressed(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+	} //end of MouseEventDemo class
+	
+} //end of window class
