5a41fd86e0521638d1d5ce136169ada8c69621a2
mzipper <markzipper99@gmail.com>
2018-04-25 23:22:44 -0400

    Added Menu. Reenabled "reactivated" speed settings. Testing Menu

--- src/main/java/com/zipper/snake/Window.java
+++ src/main/java/com/zipper/snake/Window.java
@@ -1,22 +1,43 @@
 package com.zipper.snake;
 
 import javax.swing.JFrame;
+import javax.swing.*;
+
+import java.awt.FlowLayout;
 import java.awt.event.*;
 
 class Window extends JFrame
 {
 	private static final long serialVersionUID = -2542001418764869760L;
 	
+	//Menu components
+	private JMenuBar menuBar;
+	private JMenu mainMenu;
+	private JMenu testMenu;
+	private JMenuItem newGameMenuItem;
+	private JMenuItem settingsMenuItem;
+	private JMenuItem settingsTestMI;
+	private JMenuItem gameTestMI;
 	
+	//Panels
 	private GamePlayPanel gamePanel;
+	private SettingsPanel settingsPanel;
 	
 	public Window()
 	{
-		//setLayout(new FlowLayout());
+		setLayout(new FlowLayout());
 		//setMinimumSize(new Dimension(410, 300) ); //look into panel setting abilities..
 		
+		//Create and add SettingsPanel to the GUI
+		settingsPanel = new SettingsPanel();
+		add(settingsPanel);
+		//settingsPanel.setVisible(false);
+		
+		//Create and add GamePlayPanel to the GUI
 		gamePanel = new GamePlayPanel();
 		add(gamePanel);
+		//gamePanel.setVisible(false);
+		//settingsPanel.setVisible(true);
 		
 		// Links the window to the KeyboardListener.
 		gamePanel.addKeyListener((KeyListener) new KeyboardListener());
@@ -35,7 +56,10 @@ class Window extends JFrame
 		//screenPanel.setFocusable(true);			//needed to give focus to screenPanel 
 	    addMouseListener(new ScreenMouseEvent());	//needed to return focus to screenPanel
 	    
-		
+	    
+	    buildMenu();
+	    //add(menuBar);
+	    
 		//Setting up the window settings
 		setTitle("Snake");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -86,5 +110,80 @@ class Window extends JFrame
 		}
 	} //end of ScreenMouseEvent class
 	
+	private void buildMenu()
+	{
+		//Create menuBar and Menus
+		menuBar = new JMenuBar();
+		mainMenu = new JMenu("Main Menu");
+		testMenu = new JMenu("Test Menu");
+		
+		//Add Menus to menuBar
+		menuBar.add(mainMenu);
+		menuBar.add(testMenu);
+		
+		//Create Main Menu components and add them to Main Menu
+		newGameMenuItem = new JMenuItem("New Game");
+		settingsMenuItem = new JMenuItem("Settings");
+		mainMenu.add(newGameMenuItem);
+		mainMenu.add(settingsMenuItem);
+		
+		//Create Test Menu components and add them to Test Menu
+		settingsTestMI = new JMenuItem("Settings");
+		gameTestMI = new JMenuItem("Game");
+		testMenu.add(settingsTestMI);
+		testMenu.add(gameTestMI);
+		
+		//Add MenuListener to MenuItems
+		//newGameMenuItem.addActionListener(new MenuListener() );
+		//settingsMenuItem.addActionListener(new MenuListener() );
+		settingsTestMI.addActionListener( new MenuListener() );
+		gameTestMI.addActionListener(new MenuListener() );
+		
+		//add menuBar to JFrame
+		setJMenuBar(menuBar);
+		
+	} //end of buildMenu method
 	
+	private class MenuListener implements ActionListener
+	{
+		
+		public void actionPerformed(ActionEvent e)
+		{
+			/*
+			
+			if( e.getSource().equals(newGameMenuItem) )
+			{
+				System.out.println("newGameMenuItem menu item clicked");
+			}
+			
+			if( e.getSource().equals(settingsMenuItem) )
+			{
+				System.out.println("settingsMenuItem menu item clicked");
+			}
+			*/
+			
+			if( e.getSource().equals(settingsTestMI) )
+			{
+				//System.out.println("Settings menu item clicked");
+				gamePanel.setVisible(false);
+				settingsPanel.setVisible(true);
+				gamePanel = null;
+				//gamePanel = new GamePlayPanel();
+			}
+			
+			if( e.getSource().equals(gameTestMI) )
+			{
+				//System.out.println("gameTestMI menu item clicked");
+				gamePanel = new GamePlayPanel();
+				settingsPanel.setVisible(false);
+				add(gamePanel);
+				gamePanel.addKeyListener((KeyListener) new KeyboardListener());
+				//pack();
+				gamePanel.setVisible(true);
+			}
+			
+			
+		} //end of actionPerformed method
+		
+	}
 } //end of window class
